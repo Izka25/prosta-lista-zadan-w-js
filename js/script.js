97082e8d9b1843a979b50fd3b8d5509276b3fe32
Izabela Janusz <afrodyzjak100@gmail.com>
2022-06-05 01:58:17 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,17 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+            });
+        render();
+    }
+const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+}
+
     const render = () => {
         let htmlString = "";
 
@@ -17,20 +28,24 @@
             htmlString += `
 <li
 ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+<button class="js-remove">usuń</button>
 ${task.content}
 </li>
 `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        
+        removeButtons.forEach((removeButton, index) => {
+removeButton.addEventListener("click", () => {
+    removeTask(index);
+
+});
+        });
     };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-            });
-        render();
-    }
 
     const onFormSubmit = (event) => {
         event.preventDefault();
