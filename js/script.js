b04477e2f2027c6422cf489f06ec5ca9989907e7
Izabela Janusz <afrodyzjak100@gmail.com>
2022-06-05 03:48:59 +0200

    toggle task done

--- js/script.js
+++ js/script.js
@@ -15,11 +15,17 @@
             content: newTaskContent,
             });
         render();
-    }
+    };
+
 const removeTask = (taskIndex) => {
     tasks.splice(taskIndex, 1);
     render();
 }
+
+const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+    }
 
     const render = () => {
         let htmlString = "";
@@ -28,6 +34,7 @@ const removeTask = (taskIndex) => {
             htmlString += `
 <li
 ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+<button class="js-done">zrobione?</button>
 <button class="js-remove">usuń</button>
 ${task.content}
 </li>
@@ -41,6 +48,15 @@ ${task.content}
         removeButtons.forEach((removeButton, index) => {
 removeButton.addEventListener("click", () => {
     removeTask(index);
+
+});
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+toggleDoneButton.addEventListener("click", () => {
+    toggleTaskDone(index);
 
 });
         });
