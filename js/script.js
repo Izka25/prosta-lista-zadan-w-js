f2dbc51b1a54c3f80d25e70bc686fbc6ba7f465e
Izabela Janusz <afrodyzjak100@gmail.com>
2022-06-06 01:11:21 +0200

    binding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -27,22 +27,7 @@ const toggleTaskDone = (taskIndex) => {
     render();
     }
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-<li
-${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-<button class="js-done">zrobione?</button>
-<button class="js-remove">usuń</button>
-${task.content}
-</li>
-`;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
         
         removeButtons.forEach((removeButton, index) => {
@@ -60,6 +45,25 @@ toggleDoneButton.addEventListener("click", () => {
 
 });
         });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+<li
+${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+<button class="js-done">zrobione?</button>
+<button class="js-remove">usuń</button>
+${task.content}
+</li>
+`;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
     };
 
 
